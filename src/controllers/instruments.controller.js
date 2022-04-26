28682ebc213a28f7530b26a6b13fcc904da7b705
Quavo Huncho <wartywarthog7@gmail.com>
2022-04-26 18:47:26 +0000

    whitespace and comment cleanup: #2

--- src/controllers/instruments.controller.js
+++ src/controllers/instruments.controller.js
@@ -1,34 +1,40 @@
-const { Ticker } = require("../models")
+const { Ticker } = require('../models')
 
 exports.getinstruments = async (req, res) => {
-  try {
-    const { symbol } = req.params
-    const data = await Ticker.find({ symbol: symbol }).sort({ createdOn: -1 }).limit(10)
+	try {
+		const { symbol } = req.params
+		const data = await Ticker.find({ symbol: symbol })
+			.sort({ createdOn: -1 })
+			.limit(10)
 
-    return res.status(200).send({
-      data
-    })
-  } catch (error) {
-    return res.status(500).send({
-      error: error
-    })
-  }
+		return res.status(200).send({
+			data,
+		})
+	} catch (error) {
+		return res.status(500).send({
+			error: error,
+		})
+	}
 }
 
 exports.deleteall = async (req, res) => {
-  try {
-    const foundOnes = await Ticker.find()
-    if (!foundOnes) { return res.status(400).send({ message: 'Did not find anything!' }) }
+	try {
+		const foundOnes = await Ticker.find()
+		if (!foundOnes) {
+			return res.status(400).send({ message: 'Did not find anything!' })
+		}
 
-    const deleted = await Ticker.deleteMany()
-    if(!deleted) {
-      return res.status(500).send({ message: 'Unexpected error!' })
-    }
+		const deleted = await Ticker.deleteMany()
+		if (!deleted) {
+			return res.status(500).send({ message: 'Unexpected error!' })
+		}
 
-    res.status(200).send({ message: `Deleted successfully! Total count => ${deleted.deletedCount}` })
-  } catch(error) {
-    return res.status(500).send({
-      error: error
-    })
-  } 
+		res.status(200).send({
+			message: `Deleted successfully! Total count => ${deleted.deletedCount}`,
+		})
+	} catch (error) {
+		return res.status(500).send({
+			error: error,
+		})
+	}
 }
--- src/models/ticker.js
+++ src/models/ticker.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-  symbol: String,
-  createdOn: String,
-  instType: String,
-  instId: String,
-  last: String,
-  lastSz: String,
-  askPx: String,
-  askSz: String,
-  bidPx: String,
-  bidSz: String,
-  open24h: String,
-  high24h: String,
-  low24h: String,
-  sodUtc0: String,
-  sodUtc8: String,
-  volCcy24h: String,
-  vol24h: String,
-  ts: String,
+	symbol: String,
+	createdOn: String,
+	instType: String,
+	instId: String,
+	last: String,
+	lastSz: String,
+	askPx: String,
+	askSz: String,
+	bidPx: String,
+	bidSz: String,
+	open24h: String,
+	high24h: String,
+	low24h: String,
+	sodUtc0: String,
+	sodUtc8: String,
+	volCcy24h: String,
+	vol24h: String,
+	ts: String,
 })
 
 const Ticker = mongoose.model('Ticker', schema)
 
 module.exports = {
-  Ticker
+	Ticker,
 }
